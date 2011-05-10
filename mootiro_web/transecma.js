3e09e3c61beba22c409c92c4cde676fb55133a06
Nando Florestan <nandoflorestan@gmail.com>
2011-05-10 19:53:56 -0300

    Just removing unnecessary String.prototype methods.

--- mootiro_web/transecma.js
+++ mootiro_web/transecma.js
@@ -68,20 +68,3 @@ function Transecma(tt) {
     };
     return o;
 }
-
-
-// Other String improvements
-String.prototype.contains = function (t) {
-    return this.indexOf(t) != -1;
-};
-String.prototype.endsWith = function (suffix) {
-    return this.indexOf(suffix, this.length - suffix.length) !== -1;
-};
-String.prototype.wordCount = function () {
-    var initialBlanks = /^\s+/;
-    var leftTrimmed = this.replace(initialBlanks, "");
-    var words = leftTrimmed.split(/\s+/);
-    // The resulting array may have an empty last element which must be removed
-    if (!words[words.length-1])  words.pop();
-    return words.length;
-};
