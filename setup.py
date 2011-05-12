bc26a72ea267781b79fd64e7c3630bc2fa3cfade
Nando Florestan <nandoflorestan@gmail.com>
2011-05-12 17:14:39 -0300

    setup.py bug: transecma.js was missing from tarball

--- setup.py
+++ setup.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# http://docs.python.org/whatsnew/pep-328.html
-from __future__ import absolute_import
-from __future__ import print_function   # deletes the print statement
-from __future__ import unicode_literals # unicode by default
 
 # from distutils.core import setup
 
